5c43d1dfd4747686a453358815c0f45b969f7f38
Dikimhd01 <dikihamdani.id@gmail.com>
2022-08-24 21:59:02 +0700

    Add new custom modul

---
+++ custom/estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Real State",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Diki Hamdani",
+    'category': 'Advertisement',
+    'description': """
+    This is a module for Real Estate
+    """,
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
